4d41b2e3006bd84c5436a4264ede680bdda8b62d
Dipakx <145953743+Dipakx@users.noreply.github.com>
2024-08-21 08:46:25 +0530

    Update NavBar.jsx

--- Componenant/NavBar.jsx
+++ Componenant/NavBar.jsx
@@ -1,31 +1,58 @@
-  import React from "react";
-  import "./NavBarStyle.css";
-  import { NavLink } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import "./NavBarStyle.scss";
+import { NavLink } from "react-router-dom";
 
-  function NavBar() {
-    return (
-      <>
-        <div className="Navigation">
-          <NavLink to="/">
-            <h1> Portfolio </h1>
-          </NavLink>
-          <ul className="Nav-Menu">
-            <li>
-              <NavLink to="/"> Home </NavLink>
-            </li>
-            <li>
-              <NavLink to="/About"> About </NavLink>
-            </li>
-            <li>
-              <NavLink to="/Contact"> Contact </NavLink>
-            </li>
-            <li>
-              <NavLink to="/Project"> Project </NavLink>
-            </li>
-          </ul>
-        </div>
-      </>
-    );
-  }
+function NavBar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
 
-  export default NavBar;
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const handleResize = () => {
+    setIsMobile(window.innerWidth <= 768);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <>
+      <div className="Navigation">
+        <NavLink to="/">
+          <h1> Portfolio </h1>
+        </NavLink>
+
+        {isMobile && (
+          <button className="toggle-button" onClick={toggleMenu}>
+            ☰
+          </button>
+        )}
+
+        <ul className={`Nav-Menu ${isMenuOpen ? "show" : ""}`}>
+          <li>
+            <NavLink to="/"> Home </NavLink>
+          </li>
+          <li>
+            <NavLink to="/About"> About </NavLink>
+          </li>
+          <li>
+            <NavLink to="/Contact"> Contact </NavLink>
+          </li>
+          <li>
+            <NavLink to="/Project"> Project </NavLink>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default NavBar;
